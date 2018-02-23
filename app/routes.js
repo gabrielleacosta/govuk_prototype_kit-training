725224a4c1505dde68bc24fc5ef4dc86b342893e
Gabrielle Acosta <gabrielle.acosta@digital.cabinet-office.gov.uk>
2018-02-23 15:22:23 +0000

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var router = express.Router()
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+router.post('/juggling-trick', function (req, res) {
+
+  // Make a variable and give it the value from 'juggling-balls'
+  var jugglingBalls = req.session.data['juggling-balls']
+
+  // Check whether the variable matches a condition
+  if (jugglingBalls == "3 or more"){
+    // Send user to next page
+    res.redirect('/juggling-trick')
+  } else if (jugglingBalls == "1 or 2") {
+    res.redirect('/1-or-2')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/ineligible')
+  }
+
+})
+
+
+module.exports = router
